59a123bb593b368432d131ec61db6cc413989f3a
Collin <collinli2022@gmail.com>
2021-07-24 13:06:54 -0400

    new icon and saves settings

--- background.js
+++ background.js
@@ -1,20 +1,51 @@
-var siteList = new Set(["youtube", "chess", "bit"]);
-var recent = []
+var siteList = new Set(["youtube", "chess", "bit", "colab.research"]);
+var recent_url = []
+var recent_title = []
 var max_recent = 5
 
-chrome.storage.sync.clear()
+// chrome.storage.sync.clear()
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+    console.log(
+      `Storage key "${key}" in namespace "${namespace}" changed.`,
+      `Old value was "${oldValue}", new value is "${newValue}".`
+    );
+  }
+});
+
+function updateSiteList() {
+  chrome.storage.sync.get("site_list", function(result) {
+    // console.log('Value is type of ' + typeof(siteList))
+    // console.log(siteList.size)
+    // console.log('Value is type of ' + typeof(result.site_list))
+    // console.log('Value currently is ' + result.site_list);
+    var siteList_str = [...siteList].join(' ')
+
+    if (typeof(result.site_list) == "string") {
+      new_siteList = new Set(result.site_list.split(' '))
+      // console.log(siteList.size + ' | ' + result.site_list + ' | ' + new_siteList.size)
+      // compare site_list lengths
+      if (siteList.size <= new_siteList.size) {
+        console.log('Updated site ' + result.site_list)
+        siteList = new_siteList;
+      } else {
+        chrome.storage.sync.set({"site_list": siteList_str}, function() {
+          console.log('Updated storage1 ' + siteList_str);
+        });
+      }
+    } else {
+      chrome.storage.sync.set({"site_list": siteList_str}, function() {
+        console.log('Updated storage2 ' + siteList_str);
+      });
+    }
+  });
+}
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (!tab.url.match(/^about:/)) {
 
-    chrome.storage.sync.get('site_list', function(result) {
-      console.log('Value currently is ' + result.key);
-      if (typeof(result.key) == "undefined") {
-        chrome.storage.sync.set({"site_list": siteList}, function() {
-          console.log('Value is set to ' + siteList);
-        });
-      }
-    });
+    updateSiteList()
 
     var searchingHistory = chrome.history.search({
         text: tab.url,
@@ -28,15 +59,18 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
               chrome.history.deleteUrl({
                 url: results[k].url
               }, (res) => {});
-              recent.push(results[k].url)
-              if (recent.length > max_recent) {
-                recent.shift()
+              recent_url.push(results[k].url)
+              recent_title.push(results[k].title)
+              if (recent_url.length > max_recent) {
+                recent_url.shift()
+                recent_title.shift()
               }
             }
           }
         }
       })
-    recent = [...new Set(recent)];
+      recent_url = [...new Set(recent_url)];
+      recent_title = [...new Set(recent_title)];
   }
 });
 
--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
 function updateRecents() {
   const bg = chrome.extension.getBackgroundPage()
   document.getElementById("recent").innerHTML = "<p> History for (Last 5 results) <p>"
-  for (var prop in bg.recent) {
-    document.getElementById("recent").innerHTML += "<li><a href=" + bg.recent[prop] + ">" + bg.recent[prop] + "</a></li>"
+  for (var prop in bg.recent_url) {
+    document.getElementById("recent").innerHTML += "<li><a href=" + bg.recent_url[prop] + ">" + bg.recent_title[prop] + "</a></li>"
   }
 }
 
@@ -14,39 +14,41 @@ function updateSiteList() {
   for (let i of siteListset) {
     document.getElementById("sitelist").innerHTML += "<li>" + i + "</a></li>"
   }
+
+  bg.updateSiteList()
 }
 
 window.onload = function() {
-  const bg = chrome.extension.getBackgroundPage()
-  var siteListset = bg.getSiteList()
-  chrome.storage.sync.get("data", function(items) {
-    console.log("site list: " + siteListset)
-    console.log("sync list: " + items.data);
-    if (!chrome.runtime.error) {
-      if (items.data && items.data.length >= siteListset.size) {
-        console.log(items.data.length + " - " + siteListset.size)
-        console.log("sightList updated with" + items.data)
-        bg.setSiteList(new Set(items.data))
-        document.getElementById("data").innerText = "Site List updated"
-      } else {
-        chrome.storage.sync.set({ "data" : [...siteListset] }, function() {
-          if (chrome.runtime.error) {
-            console.log("Runtime error.");
-          } else {
-            console.log("sync updated")
-            document.getElementById("data").innerText = "Sync updated"
-          }
-        });
-      }
-    }
-  });
+  // const bg = chrome.extension.getBackgroundPage()
+  // var siteListset = bg.getSiteList()
+  // chrome.storage.sync.get("data", function(items) {
+  //   console.log("site list: " + siteListset)
+  //   console.log("sync list: " + items.data);
+  //   if (!chrome.runtime.error) {
+  //     if (items.data && items.data.length >= siteListset.size) {
+  //       console.log(items.data.length + " - " + siteListset.size)
+  //       console.log("sightList updated with" + items.data)
+  //       bg.setSiteList(new Set(items.data))
+  //       document.getElementById("data").innerText = "Site List updated"
+  //     } else {
+  //       chrome.storage.sync.set({ "data" : [...siteListset] }, function() {
+  //         if (chrome.runtime.error) {
+  //           console.log("Runtime error.");
+  //         } else {
+  //           console.log("sync updated")
+  //           document.getElementById("data").innerText = "Sync updated"
+  //         }
+  //       });
+  //     }
+  //   }
+  // });
 
-  chrome.storage.sync.get("data", function(items) {
-      if (!chrome.runtime.error) {
-        console.log(items.data);
-        document.getElementById("data").innerText += "\nListed: " + items.data
-      }
-    });
+  // chrome.storage.sync.get("data", function(items) {
+  //     if (!chrome.runtime.error) {
+  //       console.log(items.data);
+  //       document.getElementById("data").innerText += "\nListed: " + items.data
+  //     }
+  //   });
   updateRecents()
   updateSiteList()
 };
@@ -57,9 +59,15 @@ document.getElementById("set").onclick = function() {
   const bg = chrome.extension.getBackgroundPage()
   var siteListset = bg.getSiteList()
   var d = document.getElementById("text").value;
-  console.log(siteListset)
+  // console.log(siteListset)
   if(siteListset.has(d)) {
     siteListset.delete(d)
+
+    // must also delete from storage
+    chrome.storage.sync.set({"site_list": siteListset}, function() {
+      bg.console.log('Deleted item from storage ' + d);
+    });
+
   } else {
     siteListset.add(d)
   }
